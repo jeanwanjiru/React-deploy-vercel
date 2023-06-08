d542abeae8c98232a13d02f7d93b3b481001a441
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-06-08 16:27:17 -0400

    initial commit

---
+++ src/Seach.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function Search({ onUserSearch }) {
+  const [search, setsearch] = useState(" ");
+  function handleChange(event) {
+    setsearch(event.target.value);
+    onUserSearch(search);
+  }
+  return (
+    <div className="ui large fluid icon input">
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="Search your Recent Transactions"
+      />
+      <i onClick={handleChange} className="circular search link icon"></i>
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/Transaction.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Transaction({ date, description, category, amount, id, onDeleteKey }) {
+  function deleteTransaction(event) {
+    onDeleteKey(event.target.id);
+  }
+  return (
+    <tr key={id}>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td>
+        <button id={id} onClick={deleteTransaction}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+export default Transaction;
